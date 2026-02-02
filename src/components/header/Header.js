f9edd5183414c541462b7d4bb19a3e33c91a26a4
JJ <jj@JJs-Mac-mini.local>
2026-02-01 23:10:20 -0800

    Fix duplicate JSX tags in Header.js by overwriting file

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -66,14 +66,18 @@ function Header(props) {
     <Fade top duration={1000} distance="20px">
       <div>
         <header className="header">
-          <input className="menu-btn" type="checkbox" id="menu-btn" />
-          <label className="menu-icon" htmlFor="menu-btn">
-            <span className="navicon"></span>
-          </label>
+          <NavLink to={link} tag={Link} className="logo">
+            <span style={{ color: theme.text }}></span>
+            <span className="logo-name" style={{ color: theme.text }}>
+              {greeting.logo_name}
+            </span>
+            <span style={{ color: theme.text }}></span>
+          </NavLink>
           <button onClick={changeTheme} className="mobile-theme-switch">
             {icon}
           </button>
-          <ul className="menu">
+          <input className="menu-btn" type="checkbox" id="menu-btn" />
+          <label className="menu-icon" htmlFor="menu-btn">
             <span className="navicon"></span>
           </label>
           <ul className="menu">
