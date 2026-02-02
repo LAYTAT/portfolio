fa7bb1a74341f9f93fef827ebad2847796e4749c
JJ <jj@JJs-Mac-mini.local>
2026-02-01 23:25:09 -0800

    Fix mobile header: align theme button left of hamburger and vertically center it

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -66,20 +66,13 @@ function Header(props) {
     <Fade top duration={1000} distance="20px">
       <div>
         <header className="header">
-          <NavLink to={link} tag={Link} className="logo">
-            <span style={{ color: theme.text }}></span>
-            <span className="logo-name" style={{ color: theme.text }}>
-              {greeting.logo_name}
-            </span>
-            <span style={{ color: theme.text }}></span>
-          </NavLink>
-          <button onClick={changeTheme} className="mobile-theme-switch">
-            {icon}
-          </button>
           <input className="menu-btn" type="checkbox" id="menu-btn" />
           <label className="menu-icon" htmlFor="menu-btn">
             <span className="navicon"></span>
           </label>
+          <button onClick={changeTheme} className="mobile-theme-switch">
+            {icon}
+          </button>
           <ul className="menu">
             <li>
               <NavLink
