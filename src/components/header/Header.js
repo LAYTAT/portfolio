250183857212633c98228a8419bdfa155c34a31e
JJ <jj@JJs-Mac-mini.local>
2026-02-01 23:33:10 -0800

    Restore Logo and fix header element order: Logo, Hamburger (edge), Theme (left of hamburger)

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -66,6 +66,13 @@ function Header(props) {
     <Fade top duration={1000} distance="20px">
       <div>
         <header className="header">
+          <NavLink to={link} tag={Link} className="logo">
+            <span style={{ color: theme.text }}></span>
+            <span className="logo-name" style={{ color: theme.text }}>
+              {greeting.logo_name}
+            </span>
+            <span style={{ color: theme.text }}></span>
+          </NavLink>
           <input className="menu-btn" type="checkbox" id="menu-btn" />
           <label className="menu-icon" htmlFor="menu-btn">
             <span className="navicon"></span>
